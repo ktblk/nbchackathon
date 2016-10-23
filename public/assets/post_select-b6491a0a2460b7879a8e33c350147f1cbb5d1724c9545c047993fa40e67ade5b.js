77a10867b6f85377ea3ecc290cb055724b451e77
Henry Arbolaez <henryarbolaez@gmail.com>
2016-10-23 06:52:41 -0400

    Precompile

---
+++ public/assets/post_select-b6491a0a2460b7879a8e33c350147f1cbb5d1724c9545c047993fa40e67ade5b.js
@@ -0,0 +1,65 @@
+
+$(document).on('click','.selected-block button.close-btn',function(event){
+	var $this = $(this),
+		$parent = $this.closest('.selected-block');
+
+	$parent.fadeOut('slow', function() {
+		$(this).remove();
+		displayHrTag()
+	});
+});
+
+$(document).on('click','.read-btn',function(event){
+	var $this = $(this),
+		$parent = $this.closest('.custom-card'),
+		options = $this.data(),
+		$cloneBlock = $('.selected-block.clone-template').clone();
+
+	$cloneBlock.removeClass('clone-template');
+
+	$cloneBlock.find('img.media-object').attr("src" , options.url);
+	$cloneBlock.find('p.text-title').text(options.title);
+	$cloneBlock.find('.media-body').data(options);
+	$this.closest('.options-block').toggleClass('active-tag');
+
+	$('.selected-section').append($cloneBlock);
+	displayHrTag()
+});
+
+$(document).on('click','button.create-video',function(event){
+	var $this = $(this),
+		$blocks = $('.selected-block:visible').find('.media-body');
+	
+	if( $blocks.length > 0 ) {
+		$(".image-view-section").empty();
+		$blocks.each(function(i,x) {
+			var img = $("<img class='img-fluid' style='max-height: 280px'>");
+			img.attr('src',  $(this).data().url);
+			$(".image-view-section").append(img);
+		});
+		$('.create-vide-section').fadeOut('fast', function() {
+			$('.image-view-section').show();
+			$('.image-view-section').slick({arrows: false});
+			$('button.create-video, .selected-section').fadeOut('slow', function() {
+				$('#accordion').fadeIn();
+			});
+			
+		});
+	} else {
+		$('.image-view-section').fadeOut('fast', function() {
+			$('.image-view-section').empty();
+			$('.create-vide-section').show();
+			$('#accordion').fadeOut();
+		});
+	}
+	
+});
+
+function displayHrTag() {
+	if( $('.selected-block').not(".clone-template").length > 0 ) {
+			$('.selected-hr').fadeIn();
+		} else {
+			$('.selected-hr').fadeOut();
+		}
+}
+;
---
+++ public/assets/telemundo-34bb37c6ee97df3ddb9a8f80e84d77b8ff393c775eeb674ad037d0dd08578ff3.js
@@ -0,0 +1,157 @@
+// $(document).ready(function(){
+// 	$('.search-button').click(function(event) {
+// 		/* Act on the event */
+// 		var $this = $(this),
+// 			$parent = $this.closest('form');
+//
+// 		$parent.find('input').addClass('show-text')
+// 	});
+//
+// 	$('.option-btn').click(function(event) {
+// 		var $this = $(this);
+// 		$this.parent().toggleClass('active-tag');
+// 	});
+//
+//
+// })
+
+$(function(){
+  videoClip();
+  article();
+  // mediaGallery();
+  ranking();
+  // brand();
+})
+
+function article() {
+
+  var description = "";
+  var photo = "";
+  var readmore = "";
+  var type = "";
+
+  $.ajax({
+    url: 'http://stage-api.nbcuni.com/telemundo/v1/content/article?limit=5',
+    type: 'GET',
+    dataType: 'JSON',
+    headers: {'api_key': document.getElementById('token').value }
+  })
+  .done(function(data) {
+    $.each(data, function(index, value){
+      // console.log(value);
+        description = value.title;
+        photo = value.cover_image.url;
+        readmore = value.url;
+        type = value.type;
+        console.log(description, photo, readmore, type);
+    })
+  });
+
+}
+
+function videoClip() {
+
+  var description = "";
+  var video = "";
+  var viewmore = "";
+  var type = "";
+
+  $.ajax({
+    url: 'http://stage-api.nbcuni.com/telemundo/v1/content/video_clip?limit=5',
+    type: 'GET',
+    dataType: 'JSON',
+    headers: {'api_key': document.getElementById('token').value }
+  })
+  .done(function(data) {
+    $.each(data, function(index, value){
+      // console.log(value);
+      description = value.title;
+      video = value.url;
+      type = value.type;
+
+      console.log(description, video, type);
+    })
+  });
+
+}
+
+
+function mediaGallery() {
+
+  var description = "";
+    var photo = "";
+    var readmore = "";
+    var type = "";
+
+  $.ajax({
+    url: 'http://stage-api.nbcuni.com/telemundo/v1/content/media_gallery?limit=5',
+    type: 'GET',
+    dataType: 'JSON',
+    headers: {'api_key': document.getElementById('token').value }
+  })
+  .done(function(data) {
+    $.each(data, function(index, value){
+      $.each(data, function(index, value){
+        // console.log(value);
+        description = value.title;
+          photo = value.cover_image.url;
+          readmore = value.url;
+          type = value.type;
+          console.log(description, photo, readmore, type);
+      })
+    })
+  });
+
+}
+
+
+
+function ranking() {
+
+  var description = "";
+    var photo = "";
+    var readmore = "";
+    var personofinterest = "";
+
+  $.ajax({
+    url: 'http://stage-api.nbcuni.com/telemundo/v1/ranking/?limit=5&sort=latest',
+    type: 'GET',
+    dataType: 'JSON',
+    headers: {'api_key': document.getElementById('token').value }
+  })
+  .done(function(data) {
+    $.each(data, function(index, value){
+      $.each(value, function(index, value){
+        // console.log(value);
+        description = value.title;
+        photo = value.photo.url;
+        readmore = value.url;
+        personofinterest = value.photo.title;
+        console.log(description, photo, readmore, personofinterest);
+      })
+    })
+  });
+
+}
+
+// function brand() {
+//
+//   var description = "";
+//     var photo = "";
+//     var readmore = "";
+//     var personofinterest = "";
+//
+//   $.ajax({
+//     url: 'http://stage-api.nbcuni.com/telemundo/v1/content/brand',
+//     type: 'GET',
+//     dataType: 'JSON',
+//     headers: {'api_key': document.getElementById('token').value }
+//   })
+//   .done(function(data) {
+//     $.each(data, function(index, value){
+//         console.log(value);
+//     })
+//   });
+//
+// }
+;
