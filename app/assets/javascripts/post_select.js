12ce8cb8cf538b6b482244ae345e32d93790f013
saccccchin <sachin@hakusports.com>
2016-10-23 06:16:14 -0400

    Front End Fix for video

--- app/assets/javascripts/post_select.js
+++ app/assets/javascripts/post_select.js
@@ -1,13 +1,13 @@
 
 $(document).on('click','.selected-block button.close-btn',function(event){
-		var $this = $(this),
-			$parent = $this.closest('.selected-block');
+	var $this = $(this),
+		$parent = $this.closest('.selected-block');
 
-		$parent.fadeOut('slow', function() {
-			$(this).remove();
-			displayHrTag()
-		});
+	$parent.fadeOut('slow', function() {
+		$(this).remove();
+		displayHrTag()
 	});
+});
 
 $(document).on('click','.read-btn',function(event){
 	var $this = $(this),
@@ -24,6 +24,35 @@ $(document).on('click','.read-btn',function(event){
 
 	$('.selected-section').append($cloneBlock);
 	displayHrTag()
+});
+
+$(document).on('click','button.create-video',function(event){
+	var $this = $(this),
+		$blocks = $('.selected-block:visible').find('.media-body');
+	
+	if( $blocks.length > 0 ) {
+		$(".image-view-section").empty();
+		$blocks.each(function(i,x) {
+			var img = $("<img class='img-fluid' style='max-height: 280px'>");
+			img.attr('src',  $(this).data().url);
+			$(".image-view-section").append(img);
+		});
+		$('.create-vide-section').fadeOut('fast', function() {
+			$('.image-view-section').show();
+			$('.image-view-section').slick({arrows: false});
+			$('button.create-video, .selected-section').fadeOut('slow', function() {
+				$('#accordion').fadeIn();
+			});
+			
+		});
+	} else {
+		$('.image-view-section').fadeOut('fast', function() {
+			$('.image-view-section').empty();
+			$('.create-vide-section').show();
+			$('#accordion').fadeOut();
+		});
+	}
+	
 });
 
 function displayHrTag() {
